f9dedcae6047303c8d94a387ad95935f80dbf500
Simon Fraser <sfraser@mozilla.com>
2020-05-14 15:18:56 +0100

    Basic artifact use

--- src/taskhuddler/task.py
+++ src/taskhuddler/task.py
@@ -1,5 +1,6 @@
 """class Task, to extract data about tasks."""
 
+from dataclasses import dataclass
 import dateutil.parser
 from taskcluster import Queue
 
@@ -9,7 +10,7 @@ from .utils import tc_options
 class TaskDefinition:
     """Data and queries about a task definition."""
 
-    def __init__(self, json=None, task_id=None, queue=None):
+    def __init__(self, task_id=None, json=None, queue=None):
         """Init."""
         # taskId is not provided in the definition
         if task_id:
@@ -64,7 +65,7 @@ class TaskDefinition:
 class TaskStatus:
     """Data and queries about a task status."""
 
-    def __init__(self, json=None, task_id=None, queue=None):
+    def __init__(self, task_id=None, json=None, queue=None):
         """Init."""
         if task_id:
             self.task_id = task_id
@@ -146,7 +147,7 @@ class TaskStatus:
     def run_durations(self):
         """Return a list of timedelta objects, of run durations."""
         durations = list()
-        if not self.json["status"].get("runs"):
+        if not self.status_json.get("runs"):
             return durations
         for run in self.status_json.get("runs", list()):
             started = run.get("started")
@@ -155,27 +156,64 @@ class TaskStatus:
                 durations.append(dateutil.parser.parse(resolved) - dateutil.parser.parse(started))
         return durations
 
+    @property
+    def latest_runid(self):
+        """Return the most recent runId."""
+        if not self.status_json.get("runs"):
+            return
+        return max([run.get("runId", 0) for run in self.status_json["runs"]])
+
+
+@dataclass
+class TaskArtifact:
+    """Understanding task arifacts."""
+
+    name: str
+    expires: str
+    storage_type: str
+    content_type: str
+    task_id: str
+    run_id: str
+
+    @classmethod
+    def from_dict(cls, data, task_id=None, run_id=None):
+        return cls(
+            task_id=task_id, run_id=run_id, name=data["name"], expires=data["expires"], storage_type=data["storageType"], content_type=data["contentType"]
+        )
+
+    def simple_name_match(self, pattern):
+        return pattern in self.name
+
+    def fetch(self, queue=None):
+        self.queue = queue
+        if self.queue is None:
+            self.queue = Queue(tc_options())
+        if self.run_id:
+            return self.queue.getArtifact(self.task_id, self.run_id, self.name)
+        else:
+            return self.queue.getLatestArtifact(self.task_id, self.name)
+
 
 class Task(TaskDefinition, TaskStatus):
     """Collected information about a single task."""
 
-    def __init__(self, json=None, task_id=None, queue=None):
+    def __init__(self, task_id=None, json=None, queue=None):
         """init."""
         if json:
             self.def_json = json.get("task")
             self.status_json = json.get("status")
             self.task_id = self.status_json["taskId"]
-            return
         if task_id:
             self.task_id = task_id
-        else:
+        if not task_id and not json:
             raise ValueError("No task definition or taskId provided")
         self.queue = queue
         if not self.queue:
             self.queue = Queue(tc_options())
-        if self.task_id:
+        if self.task_id and not json:
             self._fetch_definition()
             self._fetch_status()
+        self.artifact_store = list()
 
     def __repr__(self):
         """repr."""
@@ -194,3 +232,20 @@ class Task(TaskDefinition, TaskStatus):
     def json(self):
         """Reconstruct json as presented by listTaskGroup."""
         return {"task": self.def_json, "status": self.status_json}
+
+    def artifacts(self):
+        """List artifacts the task has produced."""
+        if not self.artifact_store:
+            list_artifacts = self.queue.listArtifacts(self.task_id, self.latest_runid, query={})
+            self.artifact_store = [TaskArtifact.from_dict(a, task_id=self.task_id, run_id=self.latest_runid) for a in list_artifacts["artifacts"]]
+        return self.artifact_store
+
+    def artifacts_matching(self, pattern):
+        for artifact in self.artifacts():
+            if pattern in artifact.name:
+                yield artifact
+
+    def fetch_artifacts_matching(self, pattern):
+        for artifact in self.artifacts_matching(pattern):
+            content = artifact.fetch()
+            print(content)
